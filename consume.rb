918d461788a06a9e989ff8fa08c6ba78e747f2ad
MARTIN Pierre <HicksCorp@GMail.com>
2016-10-15 13:13:37 +0100

    Cleans up test tokens.

--- consume.rb
+++ consume.rb
@@ -9,8 +9,6 @@ OAUTH_TOKEN_SECRET=ARGV[4]
 consumer = OAuth::Consumer.new CONSUMER_KEY, CONSUMER_SECRET, site: URL, signature_method: 'HMAC-SHA1', scheme: :header
 access_token = OAuth::AccessToken.new consumer, OAUTH_TOKEN, OAUTH_TOKEN_SECRET
 
-'7c3473ad033a46e858a63e69681daafc:b56b7d916611f2feffb7bf7e46e3120d:92fd7962cf836c206f437969337b7e15:052446145726bfaf379d3119760866a3'
-
 res = access_token.get('/api/rest/snappic/stores/current').body
 puts res
 
